e0dbacc786feb4c1f0583d267f334f89f6f454d7
Bishnu Behera <bishnu.glintlets@gmail.com>
2025-09-14 22:49:14 +0530

    mailbox ui

---
+++ src/app/features/mailbox/mailbox.routes.ts
@@ -0,0 +1,18 @@
+import { Routes } from '@angular/router';
+import { MailboxComponent } from './mailbox.component';
+
+export const MAILBOX_ROUTES: Routes = [
+    {
+        path: '',
+        component: MailboxComponent,
+        children: [
+            { path: '', redirectTo: 'inbox', pathMatch: 'full' },
+
+            {
+                path: 'inbox',
+                loadComponent: () =>
+                    import('../mailbox/inbox/inbox.component').then(m => m.InboxComponent)
+            },
+        ]
+    }
+];
